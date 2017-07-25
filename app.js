d766ed852ba891b48ea95135acc44e1d045a1707
davemflick <davemflick@yahoo.com>
2017-07-25 10:11:24 -0400

    started twitter auth

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 var passport = require('passport');
 var LocalStrategy = require("passport-local");
-var Twitter = require('passport-twitter');
+var TwitStrategy = require('passport-twitter').Strategy;
 var yelp = require('yelp-fusion');
 
 //To get rid of mongoose promise warning
@@ -24,6 +24,12 @@ mongoose.connect('mongodb://localhost/nightlife', {useMongoClient: true}, (err)=
 	}
 });
 
+//Bring routes in from 'routes' directory
+var results = require('./routes/results');
+app.use(results);
+var api = require('./routes/api');
+app.use('/api', api);
+
 //App Set Up
 app.set('view engine', 'pug');
 app.use(express.static('public'));
@@ -40,12 +46,27 @@ app.use(require("express-session")({
 //Set up Method-Override
 app.use(methodOverride("_method"));
 
+//TWITTER SET UP
+passport.use(new TwitStrategy({
+	consumerKey: process.env.TWITTER_KEY,
+	consumerSecret: process.env.TWITTER_SECRET,
+	callbackURL: 'http://localhost:3000/twitter/return'
+}, function(token, tokenSecret, profile, callback){
+	return callback(null, profile);
+}));
+
 //Set up passport
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(function(user, callback){
+	callback(null, user);
+});
+passport.deserializeUser(function(obj, callback){
+	callback(null, obj);
+});
+
+
 
 //middleware to determine if user is logged in or not, pass to every template
 app.use((req,res,next)=>{
@@ -57,100 +78,6 @@ app.use((req,res,next)=>{
 app.get('/', function(req, res, next){
 	res.render('index');
 });
-
-
-//This api gets called by axios in react.. It will return Searches collection.
-//If any of the Searches are not from the current day, it will delete them.
-//Only return Todays Searches.
-app.get('/api/results', function(req, res, next){
-	Searches.find({}, (err, city)=>{
-		if(err){
-			res.render('error', {error: err})
-		} else {
-			let curDate = new Date()
-			let today = findDate(curDate);
-			let cities = []
-			city.forEach(x=>{
-				if(today === findDate(x.timestamp)){
-					cities.push(x)
-				} else {
-					Searches.findByIdAndRemove(x._id, (err)=>{
-						if(err){
-							console.log(err);
-						} else {
-							console.log("Deleted Result with Id = " + x.id)
-						}
-					})
-				}
-			})
-			res.json({city: cities})
-		}
-	})
-
-	function findDate(date){
-		let day = date.getDate();
-		let month = date.getMonth();
-		let year = date.getFullYear();
-		return `${day}/${month}/${year}`
-	}
-})
-
-app.get('/results/:id', function(req, res, next){
-	Searches.find({}, (err, city)=>{
-		if(err){
-			res.render('error', {error: err})
-		} else {
-			res.render('index')
-		}
-	})
-})
-
-//On City Search, Run a Yelp API call, return results, create new db record of search.
-// Redirect back to results/city
-app.post('/search/:id', function(req, res, next){
-	var city = req.params.id;
-	var cityData;
-    yelp.accessToken(process.env.YELP_CLIENT_ID, process.env.YELP_CLIENT_SECRET)
-		.then((res)=>{
-			var client=yelp.client(res.jsonBody.access_token);
-			client.search({
-			  location: city,
-			  term: 'bar',
-			  limit: 2
-			}).then(response => {
-			  cityData = response.jsonBody.businesses;
-			  createCityInstance(city, cityData);
-			}).catch(e => {
-			  console.log(e);
-			});
-		}).catch((e)=>{console.log('THIS IS ERROR: ' + e)});
-	
-	function createCityInstance(location, data){
-		var establishments = [];
-		data.forEach(est=>{
-			var bar = {
-				id: est.id,
-				name: est.name,
-				image: est.image_url,
-				address: est.location.display_address,
-				rating: est.rating,
-				price: est.price,
-				peopleGoing: 0,
-				alias: est.categories
-			}
-			establishments.push(bar);
-		});
-		var citySearch = {city: location, results: establishments}
-		Searches.create(citySearch, function(err, city){
-			if(err){
-				console.log(err)
-			} else {
-				res.redirect('/results/' + req.params.id);
-			}
-		});
-	}
-});
-
 
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(err){
